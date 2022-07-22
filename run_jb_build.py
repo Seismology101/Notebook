5d6400e9edede9fc0ea703ea1a3ec94591f723d5
OUCyf <fy21@rice.edu>
2022-07-22 21:37:32 +0800

    auto

--- run_jb_build.py
+++ run_jb_build.py
@@ -1,39 +1,56 @@
-
-
+#!/Users/yinfu/opt/miniconda3/bin python3
+# -*- coding: utf-8 -*-
 import os
 import time
-from selenium import webdriver
 import subprocess
-
-driver= webdriver.Chrome() #  需要 下载 对应浏览器 驱动到 python 安装目录
-driver.get("file:///Users/yinfu/code/1-Notebook/Notebook/book/_build/html/intro.html") # 刷新网址
+from selenium import webdriver
 
 
-os.system("git switch feature_x")
+#%% Functions
+def jb_build(url, cwd_path):
+    print("Start:", time.ctime())
+    driver= webdriver.Chrome()
+    driver.get(url)
+    os.system(f"jb build {cwd_path}")
+    driver.refresh()
 
-print("Start : %s",time.ctime())
-time.sleep(0.01)
-os.system("jb build book")
-driver.refresh()
+
+def git_status(cwd_path):
+    cmd_git = subprocess.Popen(['git status -s'], shell = True, \
+        stdin = subprocess.PIPE, stdout = subprocess.PIPE, cwd = cwd_path)  
+    cmd_out_raw = cmd_git.stdout.read()
+    cmd_git.wait()
+    cmd_git.stdout.close()
+    cmd_out = cmd_out_raw.decode()
+    out = cmd_out.split("\n")
+    return out
 
 
+def git_commit():
+    os.system(""" git add . """)
+    os.system(""" git commit -m "auto" """)
 
-while True:
-    cc = subprocess.Popen(['git status -s'], shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE, cwd="./book")  
-    oo = cc.stdout.read()
-    out=oo.decode()
-    out=out.split("\n")
-    cc.wait()
-    cc.stdout.close()
 
-    print("out=",len(out))
+def main():
+    os.system("git switch dev")
+    jb_build(url, cwd_path)
+    while True: 
+        out = git_status(cwd_path)
+        # print("length of out = ", len(out))
+        if len(out) != 1:
+            jb_build(url, cwd_path)
+            git_commit()
+            time.sleep(sleep_time)
+        else:
+            continue
 
-    if len(out) != 1:
-        print("Start : %s",time.ctime())
-        time.sleep(0.01)
-        os.system("jb build book")
-        driver.refresh()
-        os.system("git add .")
-        os.system(' git commit -m "auto" ')
-    else:
-        continue
+
+#%% Main
+url = "file:///Users/yinfu/code/1-Notebook/Notebook/book/_build/html/intro.html"
+cwd_path = "./book"
+sleep_time = 0.1
+
+
+if __name__ == '__main__':
+    main()
+
